65d7e589074512be1cc7814bb6098f6957b63e3a
Ibrahim Yusuf <iy830755@gmail.com>
2024-02-08 20:51:11 +0000

    commit for checkout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import ImageCollage from './ImageCollage';
 
 import "../style/Home.css"
 import "../style/Global.css"
 
-function Home() {
-  return <>
-    <div className="mb-4">
-      <div className="container-fluid hero">
-        <NavLink 
+const Home = () => (
+  <>
+    <div className='mb-4'>
+      <div className='container-fluid hero px-0'>
+        <NavLink
           to="/react-portfolio/contact"
           end
           className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
+            isActive ? 'nav-link active px-0 pt-0' : 'nav-link px-0 pt-0'
           }
         >
+          <ImageCollage />
           <button className="btn btn-primary btn-lg" type="button" id="herogit">View Services</button>
         </NavLink>
-        
       </div>
     </div>
-      
-    <div className="container-fluid allContent">  
+
+
+    <div className="container-fluid allContent">
       <div className="row align-items-center" id="about">
         <div className="container-fluid col-lg-12 col-md-12 col-sm-12">
           <h1>Realize your vision</h1>
@@ -30,6 +32,6 @@ function Home() {
       </div>
     </div>
   </>
-}
+);
 
 export default Home;
--- src/components/ImageCollage.jsx
+++ src/components/ImageCollage.jsx
@@ -1,4 +1,17 @@
+const placeholder = "https://placehold.co/600x400";
 const ImageCollage = () => (
+    <div className="grid gap-y-1">
+        <div className="columns-3 gap-x-1">
+            <img src={placeholder} />
+            <img src={placeholder} />
+            <img src={placeholder} />
+        </div>
+        <img src={placeholder} className="w-full"/>
+        <div className="columns-2 gap-x-1">
+            <img src={placeholder} />
+            <img src={placeholder} />
+        </div>
+    </div>
 );
 
 export default ImageCollage;
