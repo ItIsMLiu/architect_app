832a9e83a785181f549ead69e479d247e1d01818
Ibrahim Yusuf <iy830755@gmail.com>
2024-02-14 12:25:00 +0000

    Use old structure

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import ImageCollage from './ImageCollage';
+import ImageCollage from "./ImageCollage";
+import { NavLink } from "react-bootstrap";
 
-import "../style/Home.css"
-import "../style/Global.css"
-
-const Home = () => (
-  <>
-    <div className="container-fluid px-0 mx-0">
+function Home() {
+  return <>
+    <div className="mb-4">
+      <div className="container-fluid hero">
+        <ImageCollage />
+      </div>
+    </div>
+      
+    <div className="container-fluid allContent">  
       <div className="row">
         <div className="container-fluid col-lg-12 col-md-12 col-sm-12 text-center" id="view-services">
           <h3>Partner with Us to Realize Your Vision</h3>
-          <NavLink
-            to="/architecture-app/services"
-            end
-            className={({ isActive }) =>
-              isActive ? 'nav-link active px-0 pt-0' : 'nav-link px-0 pt-0'
-            }
-          >
-            <ImageCollage />
-            <button className="btn btn-primary btn-lg mt-2" type="button" id="herogit">View Services</button>
-          </NavLink>
-
+          <NavLink 
+          to="/architecture-app/services"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          <button className="btn btn-primary btn-lg" type="button">View Services</button>
+        </NavLink>
         </div>
       </div>
     </div>
   </>
-);
+}
 
 export default Home;
