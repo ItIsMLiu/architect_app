c7e80774d4f8eb3c4a511ba31517c88d3485c29f
Oxanal3891 <oxanal3891@gmail.com>
2024-02-14 14:50:47 +0000

    Sorted out styling

--- src/components/Inspo/LayoutInspo.jsx
+++ src/components/Inspo/LayoutInspo.jsx
@@ -3,9 +3,11 @@ import { Outlet } from 'react-router-dom';
 import Search from './Search';
 import Nav from './NavInspo';
 import Disclaimer from './DisclaimerInspo';
+import '../../style/Inspiration.css'
+
 
 const Layout = () => (
-  <div className="App" data-testid="app-wrapper">
+  <div className="App servicesContent" data-testid="app-wrapper">
     <Search />
     <Nav />
     <Outlet />
