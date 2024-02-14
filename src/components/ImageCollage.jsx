ab54788516e0509baa43c85c1b68b0b3c29d693b
Ibrahim Yusuf <iy830755@gmail.com>
2024-02-14 13:30:14 +0000

    move tailwind css

--- src/components/ImageCollage.jsx
+++ src/components/ImageCollage.jsx
@@ -4,6 +4,7 @@ import img3 from "../assets/3.jpg";
 import img4 from "../assets/4.jpg";
 import img5 from "../assets/5.jpg";
 import img6 from "../assets/6.jpg";
+import '../style/TWImport.css';
 
 const ImageCollage = () => (
     <div className="grid gap-y-1">
